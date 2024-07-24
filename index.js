284036305d40bc8ec992cfa6cc2464cc92ae2102
MahnoorRasool <mahnoorrasool1298@gmail.com>
2024-07-24 12:27:33 +0500

    Initial commit

---
+++ index.js
@@ -0,0 +1,7 @@
+name1 = prompt("Enter your name")
+weight = prompt("Enter your weight ")
+height  = prompt("Enter your height ")
+BMI = weight / (height * height)
+document.write (name1 + "YOUR BMI IS  " + BMI)
+alert (name1 +"YOUR BMI IS  " + BMI)
+console.log  (name1 + "YOUR BMI IS " +BMI)
